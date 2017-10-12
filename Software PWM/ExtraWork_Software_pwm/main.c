65c2e3fedc836c9fc0cdca52ea9f956bebc7071b
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 21:13:05 -0400

    Update main.c

--- Software PWM/ExtraWork_Software_pwm/main.c
+++ Software PWM/ExtraWork_Software_pwm/main.c
@@ -5,16 +5,20 @@
  */
 #include <msp430.h>
 
+// Set initial duty cycle to 50%
 volatile unsigned int i = 500;
+// Initialize variable to hold button press
 volatile unsigned int btnPress = 0;
 
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
+	// LED Setup
 	P1DIR |= BIT0;
 	P4DIR |= BIT7;
 	P4OUT &= ~BIT7;
 
+	// Button Setup
 	P1DIR &= ~(BIT1); //set P1.1 input
 	P1REN |= BIT1; //enable pull-up resistor
 	P1OUT |= BIT1;
@@ -22,12 +26,14 @@ int main(void) {
 	P1IES |= ~BIT1; //set to look for falling edge
 	P1IFG &= ~(BIT1); //clear interrupt flag
 
+	// Timer A0 setup
 	TA0CCTL1 = CCIE; 			// Interrupt enabled for CCR1
 	TA0CCTL0 = CCIE;			// Interrupt enabled for CCR0
-	TA0CTL = TASSEL_2 + MC_1; 	// SMCLK, up mode
+	TA0CTL = TASSEL_2 + MC_1; 		// SMCLK, up mode
 	TA0CCR0 = 1000 - 1;			// 1 kHz frequency
 	TA0CCR1 = i - 1;			// 50% duty cycle to start
 
+	// Timer A1 setup for debounce handling
 	TA1CCR0 = 4000;
 	TA1CCTL0 |= CCIE;
 	TA1CTL |= TASSEL_2 + MC_1;
@@ -42,15 +48,15 @@ int main(void) {
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void Timer0_A0(void) {
 	if (i != 1)
-		P1OUT |= BIT0; // LED on
-	TA0CCTL0 &= ~BIT0;
+		P1OUT |= BIT0; // LED on when CCR0 hit
+	TA0CCTL0 &= ~BIT0; // Clear flag
 }
 
 #pragma vector = TIMER0_A1_VECTOR
 __interrupt void Timer0_A1(void) {
 	if (TA0CCR1 != (1000 - 1))
-		P1OUT &= ~BIT0; //LED off
-	TA0CCTL1 &= ~BIT0;
+		P1OUT &= ~BIT0; //LED off when CCR1 hit
+	TA0CCTL1 &= ~BIT0; // Clear flag
 }
 
 #pragma vector = TIMER1_A0_VECTOR
@@ -61,8 +67,11 @@ __interrupt void Timer1_Debounce(void) {
 	if (btnPress == 1) {
 		P4OUT ^= BIT7; // Toggle LED
 		if (i >= 1000) {
+			// Reset CCR1 when i goes above or equal to CCR0
 			i = 1;
 		} else {
+			// Increase by factor of SQRT(2)
+			// Linearizes brightness
 			switch (i) {
 			case 1:
 				i = 2;
@@ -125,7 +134,7 @@ __interrupt void Timer1_Debounce(void) {
 		}
 	}
 	TA0CCR1 = i - 1;
-	btnPress = 0;
+	btnPress = 0;	// Clear button press
 	P1IFG &= ~BIT1; // Clear flag
 }
 
@@ -133,7 +142,7 @@ __interrupt void Timer1_Debounce(void) {
 __interrupt void PORT_1(void) {
 	TA1CTL |= TASSEL_2 + MC_1; // Start Timer 1
 	P1IE &= ~BIT1; // Turn off interrupt enable
-	TA1CCTL0 |= CCIE; //
-	btnPress = 1;
+	TA1CCTL0 |= CCIE; // Enable timer interrupt
+	btnPress = 1; // Show button was pressed
 	P1IFG &= ~BIT1; // Clear flag
 }
