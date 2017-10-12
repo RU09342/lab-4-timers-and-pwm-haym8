c2c01abfd5dee3e33c1297109e7d300ba2039581
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 21:18:49 -0400

    Update main.c

--- Software PWM/MSP430FR6989/main.c
+++ Software PWM/MSP430FR6989/main.c
@@ -1,3 +1,10 @@
+/*
+ * Mitchell Hay
+ * RU09342
+ * Lab 4 Software PWM
+ * MSP430FR6989
+ */
+
 #include <msp430.h>
 
 volatile unsigned int i = 500;
@@ -9,11 +16,13 @@ int main(void) {
 	// Disable the GPIO power-on default high-impedance mode
 	PM5CTL0 &= ~LOCKLPM5;
 
+	// LED setup
 	P1DIR |= BIT0;
 	P1OUT &= ~BIT0;
 	P9DIR |= BIT7;
 	P9OUT &= ~BIT7;
 
+	// Button setup
 	P1DIR &= ~(BIT1); //set P1.1 input
 	P1REN |= BIT1; //enable pull-up resistor
 	P1OUT |= BIT1;
@@ -21,12 +30,14 @@ int main(void) {
 	P1IES |= ~BIT1; //set to look for falling edge
 	P1IFG &= ~(BIT1); //clear interrupt flag
 
+	// Timer A0 setup
 	TA0CCTL1 = CCIE; 			// Interrupt enabled for CCR1
 	TA0CCTL0 = CCIE;			// Interrupt enabled for CCR0
 	TA0CTL = TASSEL_2 + MC_1; 	// SMCLK, up mode
 	TA0CCR0 = 1000 - 1;			// 1 kHz frequency
 	TA0CCR1 = i - 1;			// 50% duty cycle to start
 
+	// Timer A1 setup for debounce
 	TA1CCR0 = 4000;
 	TA1CCTL0 |= CCIE;
 	TA1CTL |= TASSEL_2 + MC_1;
@@ -68,7 +79,7 @@ __interrupt void Timer1_Debounce(void) {
 		}
 	}
 	TA0CCR1 = i - 1;
-	btnPress = 0;
+	btnPress = 0; // Clear button press
 	P1IFG &= ~BIT1; // Clear flag
 }
 
@@ -76,7 +87,7 @@ __interrupt void Timer1_Debounce(void) {
 __interrupt void PORT_1(void) {
 	TA1CTL |= TASSEL_2 + MC_1; // Start Timer 1
 	P1IE &= ~BIT1; // Turn off interrupt enable
-	TA1CCTL0 |= CCIE; //
-	btnPress = 1;
+	TA1CCTL0 |= CCIE; // enable interrupt for timer
+	btnPress = 1; // Show button was pressed
 	P1IFG &= ~BIT1; // Clear flag
 }
