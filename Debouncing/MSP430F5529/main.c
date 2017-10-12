316085c08a6dc4ac8dd1a0e93c7bf0a259da0fb4
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 21:00:27 -0400

    Update main.c

--- Debouncing/MSP430F5529/main.c
+++ Debouncing/MSP430F5529/main.c
@@ -4,16 +4,16 @@
 #include <msp430.h>
 unsigned int btnPress = 0;
 
-
-
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
+	// Set up LEDs
 	P1DIR |= BIT0;
 	P1OUT &= ~BIT0;
 	P4DIR |= BIT7;
 	P4OUT &= ~BIT7;
 
+	// Set up button
 	P1DIR &= ~(BIT1); //set P1.1 input
 	P1REN |= BIT1; //enable pull-up resistor
 	P1OUT |= BIT1;
@@ -21,6 +21,7 @@ int main(void) {
 	P1IES |= ~BIT1; //set to look for falling edge
 	P1IFG &= ~(BIT1); //clear interrupt flag
 
+	// Set up Timer A for debounce handling
 	TA0CCR0 = 4000;
 	TA0CCTL0 |= CCIE;
 	TA0CTL |= TASSEL_2 + MC_1;
@@ -29,6 +30,7 @@ int main(void) {
 
 	__bis_SR_register(LPM0 + GIE); // enable interrupts in LPM0
 
+	// Loop forever
 	while (1) {}
 }
 
@@ -40,15 +42,15 @@ __interrupt void Timer0_Debounce(void) {
 	if (btnPress == 1) {
 		P4OUT ^= BIT7; // Toggle LED
 	}
-	btnPress = 0;
+	btnPress = 0;	// Clear button press
 	P1IFG &= ~BIT1; // Clear flag
 }
 
 #pragma vector = PORT1_VECTOR
 __interrupt void PORT_1(void) {
 	TA0CTL |= TASSEL_2 + MC_1; // Start Timer 1
-	P1IE &= ~BIT1; // Turn off interrupt enable
-	TA0CCTL0 |= CCIE; //
-	btnPress = 1;
+	P1IE &= ~BIT1; // Turn off interrupt enable button
+	TA0CCTL0 |= CCIE; // Turn on interrupt enable timer
+	btnPress = 1;	// Show button was pressed
 	P1IFG &= ~BIT1; // Clear flag
 }
